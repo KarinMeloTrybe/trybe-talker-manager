6543b621e85f00bb69071df33552a0aea0a84f82
Karin Melo <karinsantosmelo@gmail.com>
2023-01-07 01:02:58 -0300

    Criando o endpoint GET /talker/:id

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FIND_STATUS = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -15,6 +16,17 @@ app.get('/', (_request, response) => {
 app.get('/talker', async (_request, response) => {
   const talkersData = await getData();
   response.status(HTTP_OK_STATUS).json(talkersData);
+});
+
+app.get('/talker/:id', async (request, response) => {
+  const talker = await getData();
+  const { id } = request.params;
+  const talkerId = talker.find((person) => Number(id) === Number(person.id));
+  if (!talkerId) {
+ return response.status(HTTP_NOT_FIND_STATUS).json({
+   message: 'Pessoa palestrante não encontrada' });
+} 
+return response.status(HTTP_OK_STATUS).json(talkerId);
 });
 
 app.listen(PORT, () => {
